e0138837304269f6aa444105391b1c2367d5f586
Herschelle42 <gitcommit@threemillion.net>
2016-01-25 14:53:57 +1100

    Added PowershellHelp.ps1
    how to save and apply powershell help

---
+++ PowershellHelp.ps1
@@ -0,0 +1,24 @@
+﻿<#
+.Synopsis
+  Save Powershell Help locally for offline installation
+.Notes
+  Author:  Clint Fritz
+  Requires Admin access to update Powershell help
+#>
+
+#Set the location destination to save Powershell Help files to
+$pshelpsource = 'C:\Data\PowershellHelp'
+if (-not (Test-Path -Path $pshelpsource))
+{
+  New-Item -ItemType Directory -Path $pshelpsource
+} #end if pshelpsource
+
+#Download and Save Powershell Help
+Write-Output "Saving PowerShell help to: $($pshelpsource)"
+Save-Help -DestinationPath $pshelpsource
+
+#add test for Admin privileges
+
+#Install Powershell help files previously download
+Write-Output "Updating Powershell Help from local source: $($pshelpsource)"
+Update-Help -SourcePath $pshelpsource
