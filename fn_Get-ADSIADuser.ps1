cd4b8541879bb622be549941e685b5d914c1f27f
Herschelle42 <github@threemillion.net>
2020-03-11 16:51:14 +1100

    convert output to powershell object
    Changed the output from the raw ADSI output into a custom powershell object so that it can more easily be manipulated.

--- fn_Get-ADSIADuser.ps1
+++ fn_Get-ADSIADuser.ps1
@@ -9,20 +9,40 @@ function Get-ADSIADuser
    Only searches the current Domain this powershell session is a part of.
 .INPUTS
   [String[]]
+
 .EXAMPLE
   PS> Get-ADSIADUser -Name "Jane Doe"
 
-  Path                                         Properties
-  ----                                         ----------
-  LDAP://CN=Jane Doe,OU=Users,DC=corp,DC=local {lastlogoff, ms-ds-consistencyguid, departme...
+  accountexpires  : 9223372086854775807
+  admincount      : 1
+  adspath         : LDAP://CN=Jane Doe,OU=Users,DC=corp,DC=local
+  badpasswordtime : 132283539768796534
+  badpwdcount     : 1
+  cn              : Jane Doe
+  name            : Jane Doe
+  samaccountname  : jd001
 
 .EXAMPLE
   PS> Get-ADSIADUser -SamAccountName "jd001"
 
-  Path                                         Properties
-  ----                                         ----------
-  LDAP://CN=Jane Doe,OU=Users,DC=corp,DC=local {lastlogoff, ms-ds-consistencyguid, departme...
-  
+  accountexpires  : 9223372086854775807
+  admincount      : 1
+  adspath         : LDAP://CN=Jane Doe,OU=Users,DC=corp,DC=local
+  badpasswordtime : 132283539768796534
+  badpwdcount     : 1
+  cn              : Jane Doe
+  name            : Jane Doe
+  samaccountname  : jd001
+
+
+.EXAMPLE
+  PS> Get-ADSIADUser -SamAccountName "jd001" -Membership
+
+  Name     SamAccountName GroupName                                                
+  ----     -------        ---------                                                
+  Jane Doe jd001          App_AdobeWriter_5
+  Jane Doe jd001          Remote Desktop users
+
 #>
 [CmdletBinding(DefaultParameterSetName='Name')]
 Param
@@ -40,7 +60,11 @@ Param
                 Position=0)]
     [Alias("UserId","LogonId","Logon")]
     [String[]]
-    $SamAccountName
+    $SamAccountName,
+    [Parameter(Mandatory=$false)]
+    #Display group name membership only. 
+    [Switch]
+    $Membership=$false
 )
 
     Begin
@@ -48,13 +72,51 @@ Param
     }
     Process
     {
+        
         if ($PSCmdlet.ParameterSetName -eq "SAM")
         {
-            ([adsisearcher]"(&(objectClass=user)(samaccountname=*$SamAccountName*))").FindAll()
+            $searchResult = foreach ($itemName in $SamAccountName)
+            {
+                ([adsisearcher]"(&(objectClass=user)(samaccountname=*$itemName*))").FindAll()
+            }
         }
+
         if ($PSCmdlet.ParameterSetName -eq "Name")
         {
-            ([adsisearcher]"(&(objectClass=user)(name=*$Name*))").FindAll()
+            $searchResult = foreach ($itemName in $Name)
+            {
+                ([adsisearcher]"(&(objectClass=user)(name=*$itemName*))").FindAll()
+            }
+        }
+
+        if($Membership)
+        {
+            Write-Verbose "[INFO] Returning membership only"
+            foreach ($item in $searchResult)
+            {
+                foreach ($group in $searchResult.Properties.memberof | sort)
+                {
+                    $hash = [ordered]@{}
+                    $hash.Name = $item.Properties | % { $_.name }
+                    $hash.SamAccountName = $item.Properties | % { $_.samaccountname }
+                    $hash.GroupName = $group.Substring(3,$group.IndexOf(",")-3)
+                    $object = New-Object -TypeName PSObject -Property $hash
+                    $object
+                }#end foreach group
+            }
+        } else {
+            Write-Verbose "[INFO] Returning result as powershell object"
+            #create a custom object
+            foreach ($item in $searchResult)
+            {
+                $hash = [ordered]@{}
+                foreach ($keyName in $item.Properties.Keys | Sort)
+                {
+                    $hash.$($keyName) = $item.Properties | % { $_.$keyName }
+                }
+                $object = New-Object -TypeName PSObject -Property $hash
+                $object
+            }
         }
     }
     End
