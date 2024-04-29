67ced9645f59c3d7fc27b551d9d659c5b9a6e042
Herschelle42 <github@threemillion.net>
2024-04-29 14:55:21 +1000

    Create fn_Get-DateFromUNIXTime.ps1

---
+++ fn_Get-DateFromUNIXTime.ps1
@@ -0,0 +1,41 @@
+function Get-DateFromUNIXTime {
+<#
+.SYNOPSIS
+  Convert unix epoch time of seconds or milliseconds to a date object.
+.EXAMPLE
+  Get-DateFromUNIXTime -Milliseconds 1571702156379
+.EXAMPLE
+  Get-DateFromUNIXTime -Seconds 1568093872
+#>
+
+[CmdletBinding()]
+Param (
+    #Milliseconds from Epoch
+    [Parameter(Mandatory,
+            ValueFromPipelineByPropertyName,
+            Position=0,
+            ParameterSetName="Milliseconds")]
+    [System.Int64]$Milliseconds,
+    #Seconds from Epoch
+    [Parameter(Mandatory,
+            ValueFromPipelineByPropertyName,
+            Position=0,
+            ParameterSetName="Seconds")]
+    [System.Int64]$Seconds
+
+
+)
+
+Process {
+    #$UnixTime.Gettype()
+    if($Milliseconds) {
+        (Get-Date 1970-01-01)+([System.TimeSpan]::FromMilliseconds($Milliseconds))
+    }
+
+    if($Seconds) {
+        (Get-Date 1970-01-01)+([System.TimeSpan]::FromSeconds($Seconds))
+    }
+}
+
+}
+
